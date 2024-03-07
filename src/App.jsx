b41ded54db4ecb45ad52e7b04d28194bdf790918
ELRODRY <rodrigoezequielolivaresvcp@gmail.com>
2024-03-07 00:20:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <header>
+        <p className="tituloIzquierda">PROYECTOS WEB</p>
+        <div className="anchorsHeader">
+          <a href="" className="sobreHeader">SOBRE MI</a>
+          <a href="" className="proyectosaHeader"> PROYECTOS</a>
+          <a href="" className="contactoHeader"> CONTACTO</a>
+        </div>
+      </header>
+      <div className="sobreContainer">
+        <h1>HOLA</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
